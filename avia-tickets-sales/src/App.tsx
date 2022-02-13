d49990a14a09ff0e3375543d4b3e2bd1bf2855b1
Платонова Дарья Андреевна <plat-dasha@mail.ru>
2022-02-13 20:59:31 +0300

    added sorting

--- avia-tickets-sales/src/App.tsx
+++ avia-tickets-sales/src/App.tsx
@@ -1,13 +1,14 @@
-import { useMemo } from 'react'
+import { useEffect, useState, useMemo } from 'react'
 import { useSelector } from 'react-redux'
+
 import { RootState } from './redux/store'
 
 import Filter from './components/Filter'
 import KindSelector from './components/KindSelector'
 import Ticket from './components/Ticket'
 
-import tickets from './data/tickets.json'
 import allSegments from './data/segments.json'
+import tickets from './data/tickets.json'
 
 import { MainLogo } from './images'
 import { overlayFilterData, companyFilterData } from './constants'
@@ -16,25 +17,74 @@ import { setCompaniesFilter } from './redux/reducers/companiesFilter'
 
 import s from './App.module.scss'
 
+interface TicketType {
+  id: string;
+  price: number;
+  companyId: string;
+  segments: string[];
+}
+
 function App() {
   const { overlaysFilter, companiesFilter } = useSelector((state: RootState) => ({
     overlaysFilter: state.overlaysFilter,
     companiesFilter: state.companiesFilter,
   }))
 
-  const filteredTickets = useMemo(() => {
-    return tickets
-    .filter(ticket => companiesFilter.length ? companiesFilter.includes(ticket.companyId) : true)
-    .filter(ticket => {
+  const [sortKind, setSortKind] = useState('optimal')
+  const [dataFiltered, setDataFiltered] = useState(tickets)
+  const [dataSorted, setDataSorted] = useState(tickets)
+
+  useEffect(() => {
+    if (!(companiesFilter.length || overlaysFilter.length)) setDataFiltered(tickets)
+
+    const result = dataSorted
+    .filter((ticket) => companiesFilter.length ? companiesFilter.includes(ticket.companyId) : true)
+    .filter((ticket) => {
       const filteredSegments = allSegments
         .filter(segment => ticket.segments.includes(segment.id))
         .filter(segment =>  overlaysFilter.includes(segment.stops ? segment.stops.length.toString() : '0'))
-
+  
       return overlaysFilter.length ? filteredSegments.length !== 0 : true
     })
-  }, [companiesFilter, overlaysFilter])
 
-  const renderedTickets = (companiesFilter.length || overlaysFilter.length) ? filteredTickets : tickets
+    setDataFiltered(result)
+
+  }, [companiesFilter, overlaysFilter, dataSorted])
+
+  useEffect(() => {
+    const sortedByCheap = [...dataSorted].sort((a, b) => a.price - b.price)
+    const sortedByFast = [...dataSorted].sort((a, b) => {
+      const filteredSegmentsA = allSegments
+      .filter(segment => a.segments.includes(segment.id))
+
+      const filteredSegmentsB = allSegments
+      .filter(segment => b.segments.includes(segment.id))
+
+      const durationA = filteredSegmentsA.reduce((acc, cur) => acc+=cur.duration,0)
+      const durationB = filteredSegmentsB.reduce((acc, cur) => acc+=cur.duration,0)
+
+      return durationA - durationB
+    })
+
+    if (sortKind === 'cheap') {
+      setDataSorted(sortedByCheap)
+      return
+    }
+
+    if (sortKind === 'fast') {
+      setDataSorted(sortedByFast)
+      return
+    }
+
+    if (sortKind === 'optimal') {
+      const sortedByOptimal = sortedByCheap.map((el, i) => {
+        const fastIndex = sortedByFast.findIndex(item => item.id === el.id)
+        return { ...el, sortId: i + fastIndex }
+      }).sort((a, b) => a.sortId - b.sortId)
+
+      setDataSorted(sortedByOptimal)
+    }
+  }, [sortKind])
 
   return (
     <div className={s.root}>
@@ -57,9 +107,12 @@ function App() {
           />
         </div>
         <div className={s.results}>
-          <KindSelector />
+          <KindSelector
+            setSortKind={setSortKind}
+            sortKind={sortKind}
+          />
           <div className={s.tickets_wrapper}>
-            {renderedTickets.map(ticket => (
+            {dataFiltered.map((ticket:TicketType) => (
               <Ticket key={ticket.id} {...ticket}/>
             ))}
           </div>
--- avia-tickets-sales/src/components/KindSelector/index.tsx
+++ avia-tickets-sales/src/components/KindSelector/index.tsx
@@ -2,25 +2,41 @@ import clsx from 'clsx'
 
 import s from './KindSelector.module.scss'
 
-function KindSelector() {
+interface Props {
+  sortKind: string,
+  setSortKind: Function,
+}
+
+function KindSelector({ sortKind, setSortKind}:Props) {
 
   return (
     <div className={s.root}>
       <div
-        className={clsx(s.sector, s.sector_left)}
-        onClick={() => {}}
+        className={clsx(
+          s.sector,
+          s.sector_left,
+          sortKind === 'cheap' && s['sector-selected']
+        )}
+        onClick={() => setSortKind('cheap')}
       >
         Самый дешевый
       </div>
       <div
-        className={clsx(s.sector)}
-        onClick={() => {}}
+        className={clsx(
+          s.sector,
+          sortKind === 'fast' && s['sector-selected']
+        )}
+        onClick={() => setSortKind('fast')}
       >
         Самый быстрый
       </div>
       <div
-        className={clsx(s.sector, s.sector_right)}
-        onClick={() => {}}
+        className={clsx(
+          s.sector,
+          s.sector_right,
+          sortKind === 'optimal' && s['sector-selected']
+        )}
+        onClick={() => setSortKind('optimal')}
       >
         оптимальный
       </div>
